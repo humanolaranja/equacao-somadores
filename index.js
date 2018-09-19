eee72566a48dbae7b3733ad1eeedc49117f750d6
Humano Laranja <contato.carlos@outlook.com>
2018-09-19 00:19:26 -0300

    Working

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const _ = require('lodash');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Quantos bits terá seu somador? ', (bits) => {
+  rl.close();
+  var anterior = "c0";
+  for (var i = 0; i < bits; i++) {
+    process.stdout.write("\nc"+(parseFloat(i)+1)+" = ");
+    var array = anterior.split(" + ");
+    for (var j = 0; j < array.length; j++)
+      array[j] = "p"+i+array[j];
+    var arrayAtual = Array();
+    arrayAtual.push("g"+i);
+    arrayAtual.push(array);
+    arrayAtual = _.flattenDeep(arrayAtual);
+    var string = arrayAtual.join(" + ");
+    var anterior = string;
+    console.log(string+'\n');
+  }
+});
